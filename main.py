a623dfd3a3098876db0ce0726ac8a1d9845bf788
paco <f@hotmail.com>
2022-03-23 16:52:48 +0100

    main bueno

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 import json
 import http.client
 
+connection = http.client.HTTPConnection('api.football-data.org')
+headers = { 'X-Auth-Token': 'fe36bb2ee42647c8ac011d129de5b700' }
+connection.request('GET', 'https://api.football-data.org/v2/teams/89/matches', None, headers)
+# response = json.loads(connection.getresponse().read().decode())
+js = connection.getresponse().read().decode()
+
+response = json.loads(js)
+
+
+print(js)
